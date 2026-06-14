08e3a3b5f0286546554c18cb9543c716afb1426f
rojasespinolamario <rojasespinolamario@gmail.com>
2026-06-14 14:59:34 -0500

    feat: cree los objetos para cada Pasajero en el main class

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,14 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        HashMap<String, Pasajero> pasajeros = new HashMap<>();
+
+        Pasajero pasajero1 = new Pasajero("Juan Perez", "12345678", "Lima", "15/06/2026", "08:00", "12");
+        Pasajero pasajero2 = new Pasajero("Maria Lopez", "87654321", "Trujillo", "15/06/2026", "09:30", "18");
+        Pasajero pasajero3 = new Pasajero("Carlos Ramirez", "45678912", "Chiclayo", "16/06/2026", "07:45", "5");
+        Pasajero pasajero4 = new Pasajero("Ana Torres", "78912345", "Piura", "17/06/2026", "10:15", "22");
+        Pasajero pasajero5 = new Pasajero("Luis Garcia", "32165498", "Arequipa", "17/06/2026", "06:30", "9");
+
     }
 }
