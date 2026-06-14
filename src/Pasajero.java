8e9fd86bcac8544ca47922ccb166f5eccdb864ea
rojasespinolamario <rojasespinolamario@gmail.com>
2026-06-14 14:47:41 -0500

    feat: cree los atributos para la clase Pasajero

--- src/Pasajero.java
+++ src/Pasajero.java
@@ -1,2 +1,51 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class Pasajero {
+
+    private String nombre;
+    private String dni;
+    private String destino;
+    private String fecha;
+    private String hora;
+    private int asiento;
+
+    public Pasajero(String nombre, String dni, String destino, String fecha, String hora, int asiento) {
+        this.nombre = nombre;
+        this.dni = dni;
+        this.destino = destino;
+        this.fecha = fecha;
+        this.hora = hora;
+        this.asiento = asiento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public int getAsiento() {
+        return asiento;
+    }
+
+    @Override
+    public String toString() {
+        return "Nombre: " + nombre + "\nDNI: " + dni + "\nDestino: " + destino + "\nFecha: " + fecha + "\nHora: " + hora + "\nAsiento: " + asiento;
+    }
 }
