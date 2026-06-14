cd756c823f83c94215bc415282fe1e1f35ae6e18
rojasespinolamario <rojasespinolamario@gmail.com>
2026-06-14 15:16:58 -0500

    refactor: edite el main class, hacia falta eliminar las comillas de los asientos e importar el Hashmap y Iterator

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,11 @@ public class Main {
 
         HashMap<String, Pasajero> pasajeros = new HashMap<>();
 
-        Pasajero pasajero1 = new Pasajero("Juan Perez", "12345678", "Lima", "15/06/2026", "08:00", "12");
-        Pasajero pasajero2 = new Pasajero("Maria Lopez", "87654321", "Trujillo", "15/06/2026", "09:30", "18");
-        Pasajero pasajero3 = new Pasajero("Carlos Ramirez", "45678912", "Chiclayo", "16/06/2026", "07:45", "5");
-        Pasajero pasajero4 = new Pasajero("Ana Torres", "78912345", "Piura", "17/06/2026", "10:15", "22");
-        Pasajero pasajero5 = new Pasajero("Luis Garcia", "32165498", "Arequipa", "17/06/2026", "06:30", "9");
+        Pasajero pasajero1 = new Pasajero("Juan Perez", "12345678", "Lima", "15/06/2026", "08:00", 12);
+        Pasajero pasajero2 = new Pasajero("Maria Lopez", "87654321", "Trujillo", "15/06/2026", "09:30", 18);
+        Pasajero pasajero3 = new Pasajero("Carlos Ramirez", "45678912", "Chiclayo", "16/06/2026", "07:45", 5);
+        Pasajero pasajero4 = new Pasajero("Ana Torres", "78912345", "Piura", "17/06/2026", "10:15", 22);
+        Pasajero pasajero5 = new Pasajero("Luis Garcia", "32165498", "Arequipa", "17/06/2026", "06:30", 9);
 
         pasajeros.put(pasajero1.getDni(), pasajero1);
         pasajeros.put(pasajero2.getDni(), pasajero2);
@@ -21,7 +21,7 @@ public class Main {
 
         String dniBuscado = "45678912";
 
-        System.out.println("---- RECORRIDO DE PASAJEROS CON ITERATOR ----");
+        System.out.println("---- RECORRIDO DE PASAJEROS ----");
 
         Iterator<Map.Entry<String, Pasajero>> iterator = pasajeros.entrySet().iterator();
 
